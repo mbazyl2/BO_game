b9ae40db07214ef5e5a73f1d96d2bbb6aa443710
Marek Bazylewicz <mbazyl2@gmail.com>
2017-06-05 22:52:12 +0200

    Finished second lecture. Done some practise with canvas element.

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ ctx.rect(20, 40, 50, 50); // defining rectangle -first two parameters define the
 ctx.fillStyle = "#FF0000"; // defines color of filling
 ctx.fill();	// filling
 ctx.closePath();
+
+ctx.beginPath(); // again beignPath and closePath
+ctx.arc(240, 160 , 20 , 0, Math.PI*2, false); // first two parameters are postion of circle center, second one is radius, 0 and Math.PI*2 are start and end angle in radians, false is clockwise direction of drwaing.
+ctx.fillStyle = "green"; // defining color
+ctx.fill();	// filling circle
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0,0,255, 0.5)";
+ctx.stroke();
+ctx.closePath();
