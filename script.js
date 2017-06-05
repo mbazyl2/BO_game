5a6db53b46878c5e230c138cb17b0bb7cf2dacaa
Marek Bazylewicz <mbazyl2@gmail.com>
2017-06-05 23:05:21 +0200

    Made some annimation using canvas element.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d'); // allows to draw on canvas using 2d elements
 
+setInterval(draw, 10);
+
+var x  = canvas.width/2; // center of canvas
+var y = canvas.height - 30; 
+var dx = 2;
+var dy = -2;
+
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	x += dx;
+	y += dy;
+}
+
+/*
 ctx.beginPath(); // all instructions between ctx.beginPath(); and ctx.closePath();
 ctx.rect(20, 40, 50, 50); // defining rectangle -first two parameters define the position of top left corner on the canvas next two => width, heigth.
 ctx.fillStyle = "#FF0000"; // defines color of filling
@@ -18,3 +36,4 @@ ctx.rect(160, 10, 100, 40);
 ctx.strokeStyle = "rgba(0,0,255, 0.5)";
 ctx.stroke();
 ctx.closePath();
+*/
