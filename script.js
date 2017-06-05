1b955767a3b1626f9adaa55913691949397b3608
Marek Bazylewicz <mbazyl2@gmail.com>
2017-06-05 23:33:40 +0200

    Now ball moves only inside the canvas area.

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ var x  = canvas.width/2; // center of canvas
 var y = canvas.height - 30; 
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -20,6 +21,14 @@ function drawBall() {
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+
+	if ( y + dy < 0 || y + dy > canvas.height) {
+		dy = - dy;
+	}
+
+	if (x + dx < 0 || x + dx > canvas.width) {
+		dx = -dx;
+	}
 	x += dx;
 	y += dy;
 }
